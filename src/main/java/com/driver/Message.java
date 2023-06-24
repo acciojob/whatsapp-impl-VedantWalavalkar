20b11ea1f494250e4442fda4d3f1522be956a364
Vedant Walavalkar <vedant.walavalkar@gmail.com>
2023-06-24 16:36:09 +0530

    1st Commit

--- src/main/java/com/driver/Message.java
+++ src/main/java/com/driver/Message.java
@@ -7,4 +7,38 @@ public class Message {
     private String content;
     private Date timestamp;
 
+    Message(){
+
+    }
+    Message(int id, String content, Date timestamp){
+        this.id = id;
+        this.content = content;
+        this.timestamp = timestamp;
+    }
+    Message(String content){
+        this.content = content;
+    }
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Date timestamp) {
+        this.timestamp = timestamp;
+    }
 }
--- src/main/java/com/driver/WhatsappRepository.java
+++ src/main/java/com/driver/WhatsappRepository.java
@@ -1,5 +1,6 @@
 package com.driver;
 
+import java.text.DateFormat;
 import java.util.*;
 
 import org.springframework.stereotype.Repository;
@@ -25,5 +26,93 @@ public class WhatsappRepository {
         this.userMobile = new HashSet<>();
         this.customGroupCount = 0;
         this.messageId = 0;
+    }
+
+    public String createUser(String name, String mobile) throws Exception{
+        if(userMobile.contains(mobile)){
+            throw new Exception("User already exists");
+        }
+        User user = new User(name, mobile);
+        userMobile.add(mobile);
+        return "SUCCESS";
+    }
+
+    public Group createGroup(List<User> users) {
+        if(users.size() == 2){
+            Group group = new Group(users.get(1).getName(),2);
+            groupUserMap.put(group,users);
+            adminMap.put(group, users.get(0));
+            return group;
+        }
+        else{
+            String grpName = "Group " + customGroupCount;
+            customGroupCount++;
+            Group group = new Group(grpName, users.size());
+            groupUserMap.put(group,users);
+            adminMap.put(group,users.get(0));
+            return group;
+        }
+    }
+
+    public int createMessage(String content) {
+        Message newMsg = new Message(messageId,content, new Date(System.currentTimeMillis()));
+        messageId++;
+        return messageId;
+    }
+
+    public int sendMessage(Message message, User sender, Group group) throws Exception {
+        if(!groupUserMap.containsKey(group)){
+            throw new Exception("Group does not exist");
+        }
+        List<User> userList = groupUserMap.get(group);
+        boolean senderPresent = false;
+        for(User user:userList){
+            if(user == sender) senderPresent = true;
+        }
+        if(!senderPresent)
+        {
+            throw new Exception("You are not allowed to send message");
+        }
+        if(groupMessageMap.containsKey(group)){
+            List<Message> messageList = groupMessageMap.get(group);
+            messageList.add(message);
+            groupMessageMap.put(group,messageList);
+            message.setId(messageId);
+            message.setTimestamp(new Date(System.currentTimeMillis()));
+            messageId++;
+            return messageList.size();
+        }
+        else {
+            List<Message> newList = new ArrayList<>();
+            newList.add(message);
+            groupMessageMap.put(group, newList);
+            message.setId(messageId);
+            message.setTimestamp(new Date(System.currentTimeMillis()));
+            messageId++;
+            return 1;
+        }
+    }
+
+    public String changeAdmin(User approver, User user, Group group) throws Exception{
+        if(groupUserMap.containsKey(group))
+            throw new Exception("Group does not exist");
+        if(approver != groupUserMap.get(group).get(0))
+            throw new Exception("Approver does not have rights");
+        boolean userPresent = false;
+        int userIdx = -1;
+        List<User> userList = groupUserMap.get(group);
+        for(int i = 0 ; i<userList.size() ; i++){
+            if(userList.get(i) == user) {
+                userPresent = true;
+                userIdx = i;
+            }
+        }
+        if(!userPresent){
+            throw new Exception("User is not a participant");
+        }
+        User temp = userList.get(0);
+        userList.set(0,userList.get(userIdx));
+        userList.set(userIdx,temp);
+        return "SUCCESS";
     }
 }
